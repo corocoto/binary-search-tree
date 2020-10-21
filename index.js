f94a0d015b6e313997cf0ea4cceed4ca8e6adf53
Artem Gusev <gusev2014russia@mail.ru>
2020-10-21 21:58:56 +0300

    add jsdoc comments

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+/**
+ * Узел, используемый внутри BST.
+ * @example
+ * ```js
+ * const node = new Node(data);
+ * ```
+ */
 class Node {
+    /**
+     * @constructor
+     * @param {Number} data значение узла
+     */
     constructor(data) {
         this.data = data; //node value
         this.left = null; //left side child
@@ -6,11 +17,32 @@ class Node {
     }
 }
 
+/**
+ * Класс для создания экземпляра BST.
+ * @example
+ * ```js
+ * const BST = new BinarySearchTree();
+ * BST.insert(11);
+ * BST.insert(5);
+ * BST.insert(4);
+ * BST.insert(5);
+ * BST.insert(12);
+ * BST.insert(6);
+ * ```
+ */
 class BinarySearchTree {
+    /**
+     * @constructor
+     */
     constructor() {
         this.root = null; // bst root
     }
 
+    /**
+     * Создает экземпляр класса `Node`. И располагает его внутри BST
+     * @param {Number} data значение добавляемого узла
+     * @return {void}
+     */
     insert(data) {
         const node = new Node(data);
         if (this.root) {
@@ -20,6 +52,12 @@ class BinarySearchTree {
         }
     }
 
+    /**
+     * Размещает узел в нужной позиции BST
+     * @param {Node} node родительский узел
+     * @param {Node} newNode добавляемый узел
+     * @return {void}
+     */
     insertNode(node, newNode) {
         if (node.data > newNode.data) {
             node.left ? this.insertNode(node.left, newNode) : node.left = newNode;
@@ -28,13 +66,3 @@ class BinarySearchTree {
         }
     }
 }
-
-const BST = new BinarySearchTree();
-BST.insert(11);
-BST.insert(5);
-BST.insert(4);
-BST.insert(5);
-BST.insert(12);
-BST.insert(6);
-
-console.log(BST);
